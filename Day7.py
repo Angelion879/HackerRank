ec425ca048c0915d08015343bcf2947c14927b5c
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-01-21 14:21:52 -0300

    Day7.py

---
+++ Day7.py
@@ -0,0 +1,16 @@
+# Arrays
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    arr.reverse()
+
+    for a in range(0,n,1):
+        sys.stdout.write(str(arr[a])+ " ")
